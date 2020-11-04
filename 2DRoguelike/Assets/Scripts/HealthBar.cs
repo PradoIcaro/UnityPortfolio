6e61de28d33ee60fd7f57ab3f696f0dbca60987b
icaro_prado <icarobrook@gmail.com>
2020-11-04 16:04:51 -0300

    Created HealthBar Script

---
+++ 2DRoguelike/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]Transform fillBar;      
+    
+    public void SetBarSize(float fillValue)
+    {
+        fillBar.localScale = new Vector3(fillValue, 1f);
+    }
+    
+}
